767448baa5b3903ecba5f72c27152f7d39712229
Pontus Frendin <pontus.frendin+github@gmail.com>
2012-11-21 21:55:20 +0100

    Första versionen
    Den mest grundläggande funktionaliteten finns där. "En oslipad diamant".

---
+++ SurfinUSA.Web/CustomBootstraper.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+using Nancy.Conventions;
+public class CustomBootstrapper : DefaultNancyBootstrapper
+{
+    protected override void ConfigureConventions(NancyConventions conventions)
+    {
+        base.ConfigureConventions(conventions);
+
+        conventions.StaticContentsConventions.Add(
+            StaticContentConventionBuilder.AddFile("/index.html", "index.html")
+        );
+    }
+}
---
+++ SurfinUSA.Web/SurfModule.cs
@@ -0,0 +1,46 @@
+﻿using EasyHttp.Http;
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SurfinUSA.Web
+{
+    public class SurfModule : NancyModule
+    {
+        public SurfModule()
+        {
+            // Eftersom jag vet hur man mappar rooten till den statiska filen index.html avnäds denna redirect istället
+            Get["/"] = _ => Response.AsRedirect("/index.html", Nancy.Responses.RedirectResponse.RedirectType.SeeOther);
+
+            Get["/surf"] = _ =>
+                {
+                    Uri path;
+
+                    if (Uri.TryCreate(Request.Query.url, UriKind.Absolute, out path))
+                    {
+                        var client = new HttpClient();
+                        client.StreamResponse = true;
+                        client.Request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11";
+
+                        client.Get(path.ToString());
+
+                        using (var responseStream = client.Response.ResponseStream)
+                        {
+                            var memoryStream = new MemoryStream();
+                            responseStream.CopyTo(memoryStream);
+                            memoryStream.Position = 0;
+
+                            return Response.FromStream(memoryStream, "text/html");
+                        }
+                    }
+                    else
+                    {
+                        return Response.AsText("ogiltig url");
+                    }
+                };
+        }
+    }
+}
